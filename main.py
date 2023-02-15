1bb08c0aae5a5e8bd1aba6039f28577f09226239
Arpan Patel <arpan.patel@smu.ca>
2023-02-15 19:59:58 -0400

    Added slider

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import streamlit as st
+from joblib import dump, load
 
-st.title("Test1")
+st.title("Test")
+
+LABELS = ['setosa', 'versicolor', 'virginica'];
+clf = load("DT.joblib")
+
+sp_l = st.slider('Sepal Length: (cm)', min_value=0, max_value= 10)
+
+sp_w = st.slider('Sepal Width: (cm)', min_value=0, max_value= 10)
+
+pe_l = st.slider('Petal Length: (cm)', min_value=0, max_value= 10)
+
+pe_w = st.slider('Petal Width: (cm)', min_value=0, max_value= 10)
+
+
+prediction = clf.predict([sp_l,sp_w,pe_l,pe_w])
+
+st.write(LABELS[prediction])
+
 
